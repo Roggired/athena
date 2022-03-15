0360690de5ef425b4c1fc24a5a0352acdb406909
Roggired <leshaego14@gmail.com>
2022-03-15 18:15:11 +0300

    Initial commit

---
+++ src/test/java/ru/yofik/messanger/MessangerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.yofik.messanger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessangerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
