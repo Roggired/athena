f2ac8d8a5eff872b13e0537b7cff72fc49b01e59
ian <336546@niuitmo.ru>
2022-10-03 20:53:43 +0300

    New module "media-service" has been added

---
+++ media-service/src/main/java/ru/yofik/athena/mediaservice/MediaServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.yofik.athena.mediaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediaServiceApplication.class, args);
+	}
+
+}
