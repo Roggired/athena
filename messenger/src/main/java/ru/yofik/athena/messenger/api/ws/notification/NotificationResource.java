b1518a3a4b43edab9378fbab6d90f7d069cea153
Roggired <leshaego14@gmail.com>
2022-12-27 21:51:52 +0300

    Fix error with authentication object handling in NotificationResource

--- messenger/src/main/java/ru/yofik/athena/messenger/api/ws/notification/NotificationResource.java
+++ messenger/src/main/java/ru/yofik/athena/messenger/api/ws/notification/NotificationResource.java
@@ -1,14 +1,14 @@
 package ru.yofik.athena.messenger.api.ws.notification;
 
 import lombok.extern.log4j.Log4j2;
-import org.springframework.security.core.Authentication;
 import org.springframework.util.Assert;
 import org.springframework.web.socket.WebSocketSession;
+import ru.yofik.athena.common.security.AccessTokenAuthentication;
 import ru.yofik.athena.messenger.api.ws.AbstractWebSocketResource;
 import ru.yofik.athena.messenger.api.ws.AthenaWSMessage;
 import ru.yofik.athena.messenger.api.ws.NotificationSubscriptionKey;
 import ru.yofik.athena.messenger.api.ws.WebSocketSubscriptionType;
-import ru.yofik.athena.messenger.domain.user.model.User;
+import ru.yofik.athena.messenger.domain.user.repository.UserRepository;
 import ru.yofik.athena.messenger.infrastructure.storage.sql.user.entity.UserLastOnlineRecord;
 import ru.yofik.athena.messenger.infrastructure.storage.sql.user.repository.CrudUserLastOnlineRecordRepository;
 
@@ -18,17 +18,22 @@ import java.time.ZoneId;
 @Log4j2
 public class NotificationResource extends AbstractWebSocketResource {
     private final CrudUserLastOnlineRecordRepository crudUserLastOnlineRecordRepository;
+    private final UserRepository userRepository;
 
-    public NotificationResource(CrudUserLastOnlineRecordRepository crudUserLastOnlineRecordRepository) {
+    public NotificationResource(CrudUserLastOnlineRecordRepository crudUserLastOnlineRecordRepository, UserRepository userRepository) {
         this.crudUserLastOnlineRecordRepository = crudUserLastOnlineRecordRepository;
+        this.userRepository = userRepository;
     }
 
     @Override
     protected void handleAthenaWSMessage(WebSocketSession session, AthenaWSMessage message) throws Exception {
         log.info("Got new WS messages: " + message);
-        var authentication = (Authentication) session.getPrincipal();
+        var authentication = (AccessTokenAuthentication) session.getPrincipal();
         Assert.notNull(authentication, "ChatResource cannot get user!");
-        var user = (User) authentication.getDetails();
+        var userId = (Long) authentication.getPrincipal();
+        Assert.notNull(userId, "ChatResource cannot get user!");
+
+        var user = userRepository.getUser(userId);
         subscribe(session, new NotificationSubscriptionKey(user.getId()), WebSocketSubscriptionType.NOTIFICATION);
 
         var userLastOnlineRecord = crudUserLastOnlineRecordRepository.findByUserId(user.getId());
--- messenger/src/main/java/ru/yofik/athena/messenger/infrastructure/config/WebsocketConfig.java
+++ messenger/src/main/java/ru/yofik/athena/messenger/infrastructure/config/WebsocketConfig.java
@@ -1,21 +1,24 @@
 package ru.yofik.athena.messenger.infrastructure.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 import ru.yofik.athena.messenger.api.ws.notification.NotificationResource;
+import ru.yofik.athena.messenger.domain.user.repository.UserRepository;
 import ru.yofik.athena.messenger.infrastructure.storage.sql.user.repository.CrudUserLastOnlineRecordRepository;
 
 @Configuration
 @EnableWebSocket
+@RequiredArgsConstructor
 public class WebsocketConfig implements WebSocketConfigurer {
-    @Autowired
-    private CrudUserLastOnlineRecordRepository crudUserLastOnlineRecordRepository;
+    private final CrudUserLastOnlineRecordRepository crudUserLastOnlineRecordRepository;
+    private final UserRepository userRepository;
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(new NotificationResource(crudUserLastOnlineRecordRepository), "/ws-api/v1/notifications");
+        registry.addHandler(new NotificationResource(crudUserLastOnlineRecordRepository, userRepository), "/ws-api/v1/notifications");
     }
 }
